5ed9556fbc7aa492cecf3c3c444cdea1a1bbc2f4
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-03 15:26:49 -0600

    initialized functionality for moving rect on canvas, and set up socket connection

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,63 @@
-console.log("hay");
+"use strict";
+
+// connect to socket
+const ws = io.connect();
+
+// -----------private vars
+// postion vars
+let posX = 20;
+let posY = 20;
+
+//initial player position
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.rect(posX,posY,30,30);
+ctx.fill();
+
+
+
+//----------------  functions
+const movePlayer = (direction) => {
+  ctx.clearRect(0,0, c.width, c.height);
+  ctx.beginPath();
+  ctx.rect(posX,posY,30,30);
+  ctx.fill();
+  ctx.closePath();
+};
+
+// listen for arrow keypress
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    // move player up
+    if (e.keyCode == '38') {
+        // up arrow
+        posY -=10;
+        movePlayer("up");
+    }
+    // move player down
+    else if (e.keyCode == '40') {
+        // down arrow
+        posY += 10;
+        console.log("down");
+        movePlayer("down");
+    }
+    // move player left
+    else if (e.keyCode == '37') {
+       // left arrow
+       posX -=10;
+       console.log("left");
+       movePlayer("left");
+    }
+    // move player right
+    else if (e.keyCode == '39') {
+       // right arrow
+       posX +=10;
+       movePlayer("right");
+
+    }
+
+}
--- server.js
+++ server.js
@@ -3,6 +3,11 @@
 // --------- dependencies
 const express = require("express");
 const app = express();
+// Init naitive node server
+const server = require("http").createServer(app);
+// pass server into socketio
+const io = require("socket.io")(server);
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
@@ -12,6 +17,11 @@ app.get("/", (req, res) => {
 });
 
 // init app
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`connected on port: ${PORT}`);
+});
+
+io.on('connection', () => {
+  console.log('socket connected');
+
 });
